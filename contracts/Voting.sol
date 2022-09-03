244a3e5e8057ba07a4a7878bfc29215ff93e23b9
Riocodex <onwukachibike@gmail.com>
2022-09-03 05:20:43 +0100

    made state variables and startingelectonfunction;

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -1,8 +1,51 @@
-//SPDX-License-Identifier: Unlicense
+// SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.0;
 
-import "hardhat/console.sol";
+error Voting__SendMoreEthToVote();
+error Voting__ElectionNotOpen();
 
-contract Voting {
+contract Voting{
+    //type declarations
+    enum ElectionState{
+        OPEN,
+        BUSY
+    }
+    enum IsElection{
+        NO,
+        YES
+    }
+
+    //variables
+    uint256 private s_contractStartTime;
+    ElectionState private s_electionState;
+    IsElection private s_isElection;
+    string public votingTitle;
+    uint256 registrationPeriod;
+    uint256 electionEndingTime;
+    uint256 electionStartTime;
+
     
+
+    constructor(){
+        s_contractStartTime = block.timestamp;
+        s_electionState = ElectionState.OPEN;
+        s_isElection = IsElection.NO;
+
+    }
+
+    function startElection(string memory _title , uint256 _registrationPeriod , uint256 _endingTime)public payable{
+        if(msg.value  < 0.2 ether){
+            revert Voting__SendMoreEthToVote();
+        }
+        if(s_electionState != ElectionState.OPEN){
+            revert Voting__ElectionNotOpen();
+        }
+        votingTitle = _title;
+        registrationPeriod = _registrationPeriod;
+        electionEndingTime = _endingTime;
+        s_electionState = ElectionState.BUSY;
+        s_isElection = IsElection.YES;
+        electionStartTime = block.timestamp;
+
+    }
 }
