552032d97ed23e1ebe423cdeec2d91f59ef33213
Riocodex <onwukachibike@gmail.com>
2022-09-05 04:14:59 +0100

    created a start election and register function

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.0;
 
 error Voting__SendMoreEthToVote();
 error Voting__ElectionNotOpen();
+error Voting__RegisterationPeriodOver();
+error Voting__EndOfElection();
 
 contract Voting{
     //type declarations
@@ -10,33 +12,38 @@ contract Voting{
         OPEN,
         BUSY
     }
-    enum IsElection{
-        NO,
-        YES
+    struct CandidateDetails{
+        string name;
+        address owner;
+        uint256 numVotes;
     }
+   
+    event CandidateName(string name);
+    CandidateDetails[] public candidates;
+    mapping(string => CandidateDetails) public numToCandidate;
 
     //variables
     uint256 private s_contractStartTime;
     ElectionState private s_electionState;
-    IsElection private s_isElection;
     string public votingTitle;
     uint256 registrationPeriod;
     uint256 electionEndingTime;
     uint256 electionStartTime;
+    address owner;
+    uint256 votes;
 
     
 
     constructor(){
         s_contractStartTime = block.timestamp;
         s_electionState = ElectionState.OPEN;
-        s_isElection = IsElection.NO;
-
+        owner = msg.sender;
     }
 
-    function startElection(string memory _title , uint256 _registrationPeriod , uint256 _endingTime)public payable{
-        if(msg.value  < 0.2 ether){
-            revert Voting__SendMoreEthToVote();
-        }
+    function startElection(string memory _title , uint256 _registrationPeriod , uint256 _endingTime)public {
+        // if(msg.value  < 0.2 ether){
+        //     revert Voting__SendMoreEthToVote();
+        // }
         if(s_electionState != ElectionState.OPEN){
             revert Voting__ElectionNotOpen();
         }
@@ -44,8 +51,26 @@ contract Voting{
         registrationPeriod = _registrationPeriod;
         electionEndingTime = _endingTime;
         s_electionState = ElectionState.BUSY;
-        s_isElection = IsElection.YES;
         electionStartTime = block.timestamp;
-
     }
+    function register(string memory name)public{
+        if(s_electionState != ElectionState.BUSY){
+            revert Voting__ElectionNotOpen();
+        }
+        numToCandidate[name]=CandidateDetails(
+            name,
+            msg.sender,
+            votes
+        );
+        candidates.push(CandidateDetails(
+            name,
+            msg.sender,
+            votes
+        ));    
+    }
+
+    //view functions
+    
+    
+
 }
