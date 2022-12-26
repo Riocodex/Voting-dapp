ba4b6d27a61cb899726c41869594433fc2c6fbaa
Rio <onwukachibike@gmail.com>
2022-12-26 16:20:41 +0100

    made a test for start election

--- test/voting.test.js
+++ test/voting.test.js
@@ -1,15 +1,23 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-const tokens = (n) =>{
-    return ethers.utils.parseUnits(n.toString(), 'ether')
-}
+const money = {value: hre.ethers.utils.parseEther("10")};
 
 describe('Votingapp', ()=>{
+    let electionOwner, candidate1, candidate2, candidate3, voter1, voter2,  voter3, voter4
+    let votingContract
+
     it('saves the addresses', async() =>{
+
+        //setup accounts
+        [electionOwner, candidate1, candidate2, candidate3, voter1, voter2,  voter3, voter4] = await ethers.getSigners()
+
+        //deploy Voting app
         const VotingContract = await ethers.getContractFactory('Voting')
         votingContract = await VotingContract.deploy()
 
-        console.log(votingContract.address)
+        //start Election
+        let transaction = await votingContract.connect(electionOwner).startElection("sexiest man",20,money)
+        await transaction.wait()
     })
 })
