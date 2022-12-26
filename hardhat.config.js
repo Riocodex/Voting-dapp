f6673f79e4bd72158e6ee66a71e4d1ffcf0ff073
Rio <onwukachibike@gmail.com>
2022-12-26 15:54:52 +0100

    creating test for the smart contracts

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,10 +9,6 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY;
 module.exports = {
   solidity: "0.8.7",
   networks: {
-    goerli: {
-      url: GOERLI_URL,
-      accounts: [PRIVATE_KEY]
-    },
     hardhat: {
       chainId: 1337
     }
